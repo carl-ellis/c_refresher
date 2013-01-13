2434956d02b31d3ba43162869835bee2519487de
carl-ellis <Carlc75@gmail.com>
2013-01-13 19:40:48 +0000

    fixed spacing issues

--- partician.c
+++ partician.c
@@ -14,8 +14,8 @@ void partician(int s[], int n, int k)
 {
 	int m[MAXN+1][MAXK+1];  /* DP table for values */
 	int d[MAXN+1][MAXK+1];  /* DP table for dividers*/
-	int p[MAXN+1];						/* Prefix sums array */
-	int cost;								/* Test split costs */
+	int p[MAXN+1];					/* Prefix sums array */
+	int cost;               /* Test split costs */
 	int i,j,x;							/* counters */
 	
 	p[0] = 0;								/* construct prefix sums - sums from 0 - i in the matrix */
@@ -30,10 +30,10 @@ void partician(int s[], int n, int k)
 	{
 		for(j=2;j<=k;j++)
 		{
-			m[i][j] = MAXINT; 	/* Initial cost very high */
+			m[i][j] = MAXINT;		/* Initial cost very high */
 			for(x=1;x<=(i-1);x++) /* Check best partician costs of ranges 
-															 (range=0+x,particians=j-1)(range=n-x,particians=1) 
-															 and chose best fit */
+                            (range=0+x,particians=j-1)(range=n-x,particians=1) 
+                            and chose best fit */
 			{
 				cost = max(m[x][j-1], p[i]-p[x]);
 				if (m[i][j] > cost)
